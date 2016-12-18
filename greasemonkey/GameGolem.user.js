e1c6b041562c8d348c3bb0560e2119ae261bf704
EbilOberlord <leppykahn@yahoo.com>
2016-12-18 01:37:22 -0800

    Update GameGolem.user.js

--- greasemonkey/GameGolem.user.js
+++ greasemonkey/GameGolem.user.js
@@ -8,8 +8,8 @@
 // @include		https://web3.castleagegame.com/castle_ws/*
 // @include		http://web4.castleagegame.com/castle_ws/*
 // @include		https://web4.castleagegame.com/castle_ws/*
-// @resource		golem https://github.com/ebiloberlord/game-golem/GameGolem.js
-// @resource		stylesheet https://github.com/ebiloberlord/game-golem/golem.css
+// @resource		golem https://github.com/EbilOberlord/game-golem/blob/master/GameGolem.js
+// @resource		stylesheet https://github.com/EbilOberlord/game-golem/blob/master/golem.css
 // ==/UserScript==
 // @disabled-include		http://apps.facebook.com/castle_age/*
 // @disabled-include		https://apps.facebook.com/castle_age/*
